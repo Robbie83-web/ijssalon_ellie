65c70561e213413112985d4e31b67bd111642658
Rob Koppens <info@alien-oos.com>
2025-10-27 17:19:20 +0100

    bestand helper.py toegevoegd

---
+++ tijdelijk.py
@@ -0,0 +1,9 @@
+prijzen = {
+"aardbei" : "3",
+"vanille" : "4",
+"chocolade": "5"
+}
+aanbieding = "aardbei: 3 * [0.8]"
+reclame_tekst = "vandaag in de aanbieding", "aardbei-ijs", "1 liter-slechts","€" "[0 : 4 * 0,8]" 
+print(aanbieding)
+print(reclame_tekst)
